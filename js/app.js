66cf45fcba91937247af2f4210aecf94f08ba69f
LizbethFP <lizbeth.fp@gmail.com>
2017-12-21 10:51:20 -0500

    agregando color a círculo con jquery en función

--- js/app.js
+++ js/app.js
@@ -12,8 +12,11 @@ var eighthPicture = $('img').last();
 var firstCircle = $('i').first().next();
 var secondCircle = $('i').first().next().next();
 var thirdCircle = $('i').first().next().next().next();
-var fourthCircle = $('i').last().prev().prev();
-var fifthCircle= $('i').last().prev();
+var fourthCircle = $('i').last().prev().prev().prev().prev();
+var fifthCircle = $('i').last().prev().prev().prev();
+var sixthCircle = $('i').last().prev().prev();
+// Obtener los íconos de círculos por grupos
+var firstFourCircles = $(firstCircle).nextUntil(sixthCircle);
 // Agregar evento con función anónima
 $(firstCircle).click( function showPicture() {
   $(firstPicture).show();
@@ -24,8 +27,15 @@ $(firstCircle).click( function showPicture() {
   $(sixthPicture).hide();
   $(seventhPicture).hide();
   $(eighthPicture).hide();
+  //agregar evento para pintar círculo
+  $(event.target).css({
+    'backgroundColor':'#2ab7a9',
+    'borderRadius':'10px',
+    // 'boxShadow':'35px'
+  });
+
 });
-$(secondCircle).click( function showPicture() {
+$(secondCircle).click( function showPicture(event) {
   $(firstPicture).hide();
   $(thirdPicture).hide();
   $(fourthPicture).hide();
@@ -34,6 +44,10 @@ $(secondCircle).click( function showPicture() {
   $(seventhPicture).hide();
   $(eighthPicture).hide();
   $(secondPicture).show();
+  $(event.target).css({
+    'backgroundColor':'#2ab7a9',
+    'borderRadius':'10px'
+  });
 });
 $(thirdCircle).click( function showPicture() {
   $(firstPicture).hide();
@@ -44,6 +58,10 @@ $(thirdCircle).click( function showPicture() {
   $(seventhPicture).hide();
   $(eighthPicture).hide();
   $(thirdPicture).show();
+  $(event.target).css({
+    'backgroundColor':'#2ab7a9',
+    'borderRadius':'10px'
+  });
 });
 $(fourthCircle).click( function showPicture() {
   $(firstPicture).hide();
@@ -54,6 +72,10 @@ $(fourthCircle).click( function showPicture() {
   $(seventhPicture).hide();
   $(eighthPicture).hide();
   $(fourthPicture).show();
+  $(event.target).css({
+    'backgroundColor':'#2ab7a9',
+    'borderRadius':'10px'
+  });
 });
 $(fifthCircle).click( function showPicture() {
   $(firstPicture).hide();
@@ -64,6 +86,10 @@ $(fifthCircle).click( function showPicture() {
   $(seventhPicture).hide();
   $(eighthPicture).hide();
   $(fifthPicture).show();
+  $(event.target).css({
+    'backgroundColor':'#2ab7a9',
+    'borderRadius':'10px'
+  });
 });
 // Cierre de función general
 });
